65c2d0b23ce7de45e40962831844f456cb35da93
Shaun Tseng <shaun92168@gmail.com>
2020-11-03 01:42:55 -0800

    Add floor and enemy with simple AI

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 0.1f;
+    private float rotAmount;
+    // private Rigidbody rb;
+    private Vector3 movement;
+    private Vector3 rvec;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // rb = GetComponent<Rigidbody>();
+        rvec = new Vector3(0,1,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector3(0,0,-1);
+        Debug.Log("213");
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector3 direction)
+    {
+        // rb.MovePosition(transform.position + (direction * speed * Time.deltaTime));
+        transform.Translate(direction * speed * Time.deltaTime);
+
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        if(col.collider.name == "Quad4" || col.collider.name == "Quad1" || col.collider.name == "Quad2" || col.collider.name == "Quad3")
+        {
+            rotAmount = Random.Range(90, 270);
+            transform.Rotate(rvec, rotAmount);
+        }
+    } 
+
+    private Vector3 RandomVector(float min, float max) {
+        var x = Random.Range(min, max);
+        var y = Random.Range(min, max);
+        var z = Random.Range(min, max);
+        return new Vector3(x, y, z);
+    }
+}
