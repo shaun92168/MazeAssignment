6eedeb12b449a2a312dcc8806addb867ec13d5b5
Jaedin D <jaedindhatt@gmail.com>
2020-11-07 15:01:04 -0800

    toggle wall collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public CharacterController controller;
     public GameObject WinScreen;
-    public float speed = 12f; 
+    public float speed = 12f;
+    public GameObject[] Walls;
+    public Text toggleText;
+    private bool toggleStatus;
+    private IEnumerator coroutine;
+
     void Start()
     {
+        toggleText.enabled = false;
+        Walls = GameObject.FindGameObjectsWithTag("Walls");
         WinScreen.SetActive(false);
     }
 
@@ -26,6 +34,16 @@ public class PlayerController : MonoBehaviour
         {
             SceneManager.LoadScene("SampleScene");
         }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ToggleWalls();
+        }
+
+        if (this.gameObject.transform.position.y != 0.27)
+        {
+            this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, 0.27f, this.gameObject.transform.position.z);
+        }
     }
 
     void OnTriggerEnter(Collider col)
@@ -36,5 +54,32 @@ public class PlayerController : MonoBehaviour
         }
         
         Debug.Log(col.name);
+    }
+
+    void ToggleWalls()
+    {
+        foreach (GameObject wall in Walls)
+        {
+            wall.gameObject.GetComponent<Collider>().enabled = !wall.gameObject.GetComponent<Collider>().enabled;
+            toggleStatus = wall.gameObject.GetComponent<Collider>().enabled;
+        }
+
+        if(toggleStatus)
+        {
+            toggleText.text = "Wall Collision Enabled";
+        } else
+        {
+            toggleText.text = "Wall Collision Disabled";
+        }
+
+        coroutine = ShowMessage(toggleText.text, 1.0f);
+        StartCoroutine(coroutine);
+
+        IEnumerator ShowMessage (string message, float delay)
+        {
+            toggleText.enabled = true;
+            yield return new WaitForSeconds(delay);
+            toggleText.enabled = false;
+        }
     }
 }
