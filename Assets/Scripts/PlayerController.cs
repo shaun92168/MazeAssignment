ef83e5b592b36c427094a0c11b1a3f4b411023a7
Jaedin D <jaedindhatt@gmail.com>
2020-12-02 20:16:12 -0800

    esc menu + save function for score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
@@ -18,9 +21,27 @@ public class PlayerController : MonoBehaviour
     private bool toggleStatus = true;
     public Text scoreText;
     public int score;
+    public Canvas escMenu;
+    private bool paused = false;
     private IEnumerator coroutine;
     private GameObject PongEntrance;
 
+    public static PlayerController pCtrl;
+    private Vector3 playerPos;
+    private Vector3 enemyPos;
+    private int playerScore;
+    const string fileName = "/mazeSave.dat";
+
+    private void Awake()
+    {
+        if (pCtrl == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            pCtrl = this;
+            pCtrl.score = 0;
+            LoadState();
+        }
+    }
     void Start()
     {
         GameObject enemy = Instantiate(Enemy, Enemy.transform.position, Enemy.transform.rotation);
@@ -29,7 +50,6 @@ public class PlayerController : MonoBehaviour
         Walls = GameObject.FindGameObjectsWithTag("Walls");
         PongEntrance = GameObject.FindGameObjectWithTag("Pong");
         WinScreen.SetActive(false);
-        score = 0;
     }
 
     void Update()
@@ -41,25 +61,45 @@ public class PlayerController : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetKeyDown(KeyCode.Home))
+        if (Input.GetKeyDown(KeyCode.Home))
         {
-            Cursor.lockState = CursorLockMode.Locked; 
+            Cursor.lockState = CursorLockMode.Locked;
             SceneManager.LoadScene("SampleScene");
             Time.timeScale = 1;
         }
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            toggleEscMenu();
+        }
+
+        void toggleEscMenu()
+        {
+            if (paused)
+            {
+                Time.timeScale = 1;
+                escMenu.gameObject.SetActive(false);
+                Cursor.lockState = CursorLockMode.Locked;
+            } else {
+                Time.timeScale = 0;
+                escMenu.gameObject.SetActive(true);
+                Cursor.lockState = CursorLockMode.None;
+            }
+            paused = !paused;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             ToggleWalls();
         }
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Time.timeScale != 0)
         {
             GameObject thrownBall = Instantiate(Ball, BallSpawn.position, BallSpawn.rotation);
             thrownBall.GetComponent<Rigidbody>().velocity = thrownBall.transform.forward * ballSpeed;
         }
 
-        scoreText.text = "Score: " + score;
+        scoreText.text = "Score: " + pCtrl.score;
 
         if (this.gameObject.transform.position.y != 0.27)
         {
@@ -69,19 +109,19 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerEnter(Collider col)
     {
-        if(col.name == "END")
+        if (col.name == "END")
         {
             WinScreen.SetActive(true);
             Cursor.lockState = CursorLockMode.None;
             Time.timeScale = 0;
         }
 
-        if(col.name == "DoorB")
+        if (col.name == "DoorB")
         {
             Debug.Log("Enter Pong");
             SceneManager.LoadScene("PongAI", LoadSceneMode.Single);
         }
-        
+
         Debug.Log(col.name);
     }
 
@@ -97,7 +137,8 @@ public class PlayerController : MonoBehaviour
         if (toggleStatus)
         {
             toggleText.text = "Wall Collision Enabled";
-        } else
+        }
+        else
         {
             toggleText.text = "Wall Collision Disabled";
         }
@@ -105,11 +146,39 @@ public class PlayerController : MonoBehaviour
         coroutine = ShowMessage(toggleText.text, 1.0f);
         StartCoroutine(coroutine);
 
-        IEnumerator ShowMessage (string message, float delay)
+        IEnumerator ShowMessage(string message, float delay)
         {
             toggleText.enabled = true;
             yield return new WaitForSeconds(delay);
             toggleText.enabled = false;
         }
     }
+
+    public void LoadState()
+    {
+        if (File.Exists(Application.persistentDataPath + fileName))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = File.Open(Application.persistentDataPath + fileName, FileMode.Open, FileAccess.Read);
+            GameData data = (GameData)bf.Deserialize(fs);
+            fs.Close();
+            pCtrl.score = data.score;
+        }
+    }
+
+    public void SaveState()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream fs = File.Open(Application.persistentDataPath + fileName, FileMode.OpenOrCreate);
+        GameData data = new GameData();
+        data.score = pCtrl.score;
+        bf.Serialize(fs, data);
+        fs.Close();
+    }
+}
+
+[Serializable]
+class GameData
+{
+    public int score;
 }
---
+++ Assets/Scripts/SaveButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveButton : MonoBehaviour
+{
+    PlayerController PlayerController;
+
+    private void Start()
+    {
+        PlayerController = FindObjectOfType<PlayerController>();
+    }
+
+    public void SaveState()
+    {
+        PlayerController.SaveState();
+    }
+}
